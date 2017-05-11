5aad6776c2bccc7857229dfeb3d13a48329ed800
Your Name <you@example.com>
2017-05-11 15:11:40 +0800

    加入專案檔案。

---
+++ WebApplication/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult Logon()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LogonViewModel pageData)
+        {
+            var status = "";
+            if (pageData.Account == "skill" && pageData.Password == "tree")
+            {
+                pageData.Message = $"您使用{pageData.Account}登入成功。";
+                status = "alert-success";
+                //pageData.Status = $"alert-success";
+            }
+            else
+            {
+                pageData.Message = $"您使用{pageData.Account}登入失敗。";
+                status = "alert-danger";
+                //pageData.Status = $"alert-danger";
+            }
+            ViewData["Status"] = string.Format("<div class=\"alert {0} \">{1}</div>", status, pageData.Message);
+            return View(pageData);
+        }
+    }
+}
---
+++ WebApplication/Models/ViewModels/LogonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.ViewModels
+{
+    public class LogonViewModel
+    {
+        
+        
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+        
+    }
+}
